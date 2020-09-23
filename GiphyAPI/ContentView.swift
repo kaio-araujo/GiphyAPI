caf04a8e5ea5e81e92424bd3425139496cc2b129
Kaio <kaioaguanais@gmail.com>
2020-09-23 14:29:00 -0700

    Initial Commit

---
+++ GiphyAPI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GiphyAPI
+//
+//  Created by Kaio Guanais on 2020-09-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GiphyAPI/GiphyAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GiphyAPIApp.swift
+//  GiphyAPI
+//
+//  Created by Kaio Guanais on 2020-09-23.
+//
+
+import SwiftUI
+
+@main
+struct GiphyAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
